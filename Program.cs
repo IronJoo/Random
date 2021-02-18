c23d78178a1e66de0cf02365e96d6e2255b2e9e0
IronJoo <joanaferro92@gmail.comgit commit -m First>
2021-02-18 18:33:42 +0000

    First Commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Testes
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Are you male? (Insert 'true' or 'false'): ");
+            bool isMale = Convert.ToBoolean(Console.ReadLine());
+            Console.Write("Are you tall? (Insert 'true' of 'false'): ");
+            bool isTall = Convert.ToBoolean(Console.ReadLine());
+
+            if (isMale && isTall)
+            {
+                Console.WriteLine("You are male and you are tall!");
+            }
+            else
+            {
+                if (!isMale && isTall)
+                {
+                    Console.WriteLine("You are not male and you are tall!");
+                }
+                else
+                {
+                    if (!isTall && isMale)
+                    {
+                        Console.WriteLine("You are male and you are not tall!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You are not male and you are not tall!");
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
