00230c9ba415c0a5fe26239724ce3b00416b74b5
IronJoo <joanaferro92@gmail.comgit commit -m First>
2021-02-19 23:10:27 +0000

    Factorial

--- Program.cs
+++ Program.cs
@@ -6,52 +6,20 @@ namespace Testes
     {
         public static void Main(string[] args)
         {
-            Console.Write("Are you male?: ");
-            string temp = Console.ReadLine();
-            bool isMale = true;
-            bool isTall = true;
-            if (temp.ToLower().Equals("yes"))
+            Console.Write("Insert a number: ");
+            Console.Write(Factorial(int.Parse(Console.ReadLine())));
+
+            Console.ReadLine();
+        }
+        static int Factorial(int num)
+        {
+            int result = num;
+            for (int i = num; i > 1; i--)
             {
-                isMale = true;
-            }
-            else if (temp.ToLower().Equals("no"))
-            {
-                isMale = false;
-            }
-            Console.Write("Are you tall?: ");
-            string temp2 = Console.ReadLine();
-            if (temp2.ToLower().Equals("yes"))
-            {
-                isTall = true;
-            }
-            else if (temp2.ToLower().Equals("no"))
-            {
-                isTall = false;
+                result = result * (i - 1);
             }
 
-            if (isMale && isTall)
-            {
-                Console.WriteLine("You are male and you are tall!");
-            }
-            else
-            {
-                if (!isMale && isTall)
-                {
-                    Console.WriteLine("You are not male and you are tall!");
-                }
-                else
-                {
-                    if (!isTall && isMale)
-                    {
-                        Console.WriteLine("You are male and you are not tall!");
-                    }
-                    else
-                    {
-                        Console.WriteLine("You are not male and you are not tall!");
-                    }
-                }
-            }
-            Console.ReadLine();
+            return result;
         }
     }
 }
