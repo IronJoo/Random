d1e92fe995f4e09fb9c224e05cbe8340d2555818
IronJoo <joanaferro92@gmail.comgit commit -m First>
2021-02-20 22:22:51 +0000

    Highest from 3 inputs

--- Program.cs
+++ Program.cs
@@ -10,21 +10,27 @@ namespace Testes
             int num1 = int.Parse(Console.ReadLine());
             Console.Write("Insert another number: ");
             int num2 = int.Parse(Console.ReadLine());
-            Console.WriteLine($"You inserted the numbers {num1} and {num2}.");
-            Console.WriteLine("The highest of them is {0}.", Highest(num1, num2));
+            Console.Write("Insert one more number: ");
+            int num3 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"You inserted the numbers {num1}, {num2} and {num3}.");
+            Console.WriteLine("The highest of them is {0}.", Highest(num1, num2, num3));
             
 
             Console.ReadLine();
         }
-        static int Highest(int num1, int num2)
+        private static int Highest(int num1, int num2, int num3)
         {
-            if (num1 > num2)
+            if (num1 >= num2 && num1 >= num3)
             {
                 return num1;
             }
-            else
+            else if (num2 >= num1 && num2 >= num3)
             {
                 return num2;
+            }
+            else
+            {
+                return num3;
             }
         } 
     }
