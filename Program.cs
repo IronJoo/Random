cd3162ab22ef62ee1dbf2fc659c0adb8d5155dd8
IronJoo <joanaferro92@gmail.com>
2021-02-25 17:43:49 +0000

    Guessing Game 2.0

--- Program.cs
+++ Program.cs
@@ -2,24 +2,46 @@
 
 namespace Testes
 {
-    class MainClass //{}.
+    class MainClass //{}
     {
         public static void Main(string[] args)
         {
-            string secretWord = "giraffe";
+            Console.Write("Insert a number to be guessed by your partner!: ");
+            int secretNum = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Guess what word I'm thinking of!: ");
-            string guess = Console.ReadLine();
-            while (secretWord != guess)
+            Console.Clear();
+
+            Console.WriteLine("Your number is secret! You may now pass your device to your partner.");
+            Console.WriteLine("Press ENTER to begin.");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("From 1 to 10, guess what number I'm thinking of!: ");
+            int guess = int.Parse(Console.ReadLine());
+            int guessCount = 1;
+
+            if (guess == secretNum)
             {
-                Console.WriteLine("Incorrect! Try again: ");
-                guess = Console.ReadLine();
+                Console.WriteLine("Congratulations, you got it on your first attempt!");
             }
-
-            Console.WriteLine("Congratulations, you got it!");
-
-
-
+            else
+            {
+                while (guessCount < 3 && guess != secretNum)
+                {
+                    Console.WriteLine("Incorrect! Try again: ");
+                    guess = int.Parse(Console.ReadLine());
+                    guessCount++;
+                    if (guess == secretNum)
+                    {
+                        Console.WriteLine("Congratulations, you got it!");
+                    }
+                    else if (guessCount == 3 && guess != secretNum)
+                    {
+                        Console.WriteLine("You ran out of attempts.");
+                    }
+                }
+            }
+                
             Console.ReadLine();
         } 
     }
