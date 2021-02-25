6c306e8fe8a1ffb85e2b87ffd2fc8da1ecae7bd7
IronJoo <joanaferro92@gmail.com>
2021-02-25 16:44:47 +0000

    Guessing Game

--- Program.cs
+++ Program.cs
@@ -6,34 +6,19 @@ namespace Testes
     {
         public static void Main(string[] args)
         {
-            Console.Write("Insert a number: ");
-            double num1 = double.Parse(Console.ReadLine());
-            Console.Write("Insert an operator: ");
-            string op = Console.ReadLine();
-            Console.Write("Insert another number: ");
-            double num2 = double.Parse(Console.ReadLine());
-            Console.Write($"You inserted the following calculation: {num1} {op} {num2} = ");
-            if (op == "x" || op == "*")
+            string secretWord = "giraffe";
+
+            Console.WriteLine("Guess what word I'm thinking of!: ");
+            string guess = Console.ReadLine();
+            while (secretWord != guess)
             {
-                Console.Write(num1 * num2);
+                Console.WriteLine("Incorrect! Try again: ");
+                guess = Console.ReadLine();
             }
-            else if (op == "+")
-            {
-                Console.Write(num1 + num2);
-            }
-            else if (op == "-")
-            {
-                Console.Write(num1 - num2);
-            }
-            else if (op == ":" || op == "/")
-            {
-                double result = num1 / num2;
-                Console.Write(result);
-            }
-            else
-            {
-                Console.Write("Invalid operator.");
-            }
+
+            Console.WriteLine("Congratulations, you got it!");
+
+
 
             Console.ReadLine();
         } 
