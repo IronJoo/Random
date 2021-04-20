66147b9ca580787898fd9d0f0e9f52bbf08925ab
Joana Ferro <joanaferro92@gmail.com>
2021-04-20 19:54:45 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,29 +1,33 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 
 namespace Testes
 {
-    class MainClass //{}
+    class MainClass
     {
         public static void Main(string[] args)
         {
             Console.Write("Fibonacci Sequence\nHow many times do you want to run the sequence for?: ");
             int x = int.Parse(Console.ReadLine());
-            Console.WriteLine(Fibonacci(x));
-
-
+            int[] result = Fibonacci(x);
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine(result[i]);
+            }
         }
-        int Fibonacci(int nTimes)
+        static int[] Fibonacci(int nTimes)
         {
             int num1 = 0;
             int num2 = 1;
-            int result;
-            for (int i = 1; i < nTimes; i++)
+            List<int> result = new List<int>();
+            for (int i = 0; i < nTimes; i++)
             {
-                result = num1 + num2;
+                result.Add(num1 + num2);
                 num1 = num2;
-                num2 = result;
+                num2 = result[i];
             }
             return result;
+
         }
     }
 }
